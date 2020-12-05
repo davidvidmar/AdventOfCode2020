a7e34697c3e35948549579b5a03ac25f57930b9d
David <info@vidmar.net>
2020-12-05 11:05:09 +0100

    Day 4

---
+++ Day04/Passport.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Day04
+{
+    class Passport
+    {
+        public int? BirthYear;
+        public int? IssueYear;
+        public int? ExpirationYear;
+        public string Height;
+        public string HairColor;
+        public string EyeColor;
+        public string PassportID;
+        public string CountryID;
+
+        public bool IsValid(bool simple = true)
+        {
+            var valid = 
+                BirthYear != null &&
+                IssueYear != null &&
+                ExpirationYear != null &&
+                Height != null &&
+                HairColor != null &&
+                EyeColor != null &&
+                PassportID != null;
+
+            if (valid && !simple)
+            {
+                valid &= BirthYear >= 1920 && BirthYear <= 2002;
+                valid &= IssueYear >= 2010 && IssueYear <= 2020;
+                valid &= ExpirationYear >= 2020 && ExpirationYear <= 2030;
+
+                var unit = Height[^2..];
+                valid &= Int32.TryParse(Height[0..^2], out int h);
+                if (unit == "cm") valid &= h >= 150 && h <= 193;
+                else if (unit == "in") valid &= h >= 59 && h <= 76;
+                else valid = false;
+
+                valid &= HairColor[0] == '#' && long.TryParse(HairColor[1..], System.Globalization.NumberStyles.HexNumber, null, out long hcl);
+
+                valid &= (new[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" }).Contains(EyeColor);
+
+                valid &= PassportID.Length == 9 && long.TryParse(PassportID, out long pid);
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,77 @@
+﻿using Day04;
+using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result1 = 0;
+            int result2 = 0;
+            int count = 0;
+            
+            Utils.StartDay(4);
+            Utils.StartPart(1);
+
+            var linesSample = Utils.ReadInputAsLines("input-sample.txt");
+            count = CountValidPassports(linesSample);
+            Console.WriteLine("Sample: " + count);
+
+            var lines = Utils.ReadInputAsLines();
+            result1 = CountValidPassports(lines);
+            Utils.EndPart(1, result1);
+
+            Utils.StartPart(2);
+
+            linesSample = Utils.ReadInputAsLines("input-sample2-valid.txt");
+            count = CountValidPassports(linesSample, false);
+            Console.WriteLine("Sample 2 - Valid: " + count);
+
+            linesSample = Utils.ReadInputAsLines("input-sample2-invalid.txt");
+            count = CountValidPassports(linesSample, false);
+            Console.WriteLine("Sample 2 - Invalid: " + count);
+
+            result2 = CountValidPassports(lines, false);
+
+            Utils.EndPart(2, result2);
+
+            Utils.Wait();
+        }
+
+        private static int CountValidPassports(string[] lines, bool simple = true)
+        {
+            var count = 0;
+            var passport = new Passport();
+            foreach (var line in lines)
+            {
+                if (line.Length == 0)
+                {
+                    if (passport.IsValid(simple)) count++;
+                    passport = new Passport();
+                    continue;
+                }
+                var fields = line.Split(' ');
+                foreach (var field in fields)
+                {
+                    var kv = field.Split(':');
+                    switch (kv[0])
+                    {
+                        case "byr": passport.BirthYear = Convert.ToInt32(kv[1]); break;
+                        case "iyr": passport.IssueYear = Convert.ToInt32(kv[1]); break;
+                        case "eyr": passport.ExpirationYear = Convert.ToInt32(kv[1]); break;
+                        case "hgt": passport.Height = kv[1]; break;
+                        case "hcl": passport.HairColor = kv[1]; break;
+                        case "ecl": passport.EyeColor = kv[1]; break;
+                        case "pid": passport.PassportID = kv[1]; break;
+                        case "cid": passport.CountryID = kv[1]; break;
+                        default:
+                            throw new Exception($"Invalid key: {kv}!");
+                    }
+                }
+            }
+            if (passport.IsValid(simple)) count++;
+            return count;
+        }
+    }
+}
